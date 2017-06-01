90fb82c87b7e91e99fbf4ea3cdbb55b77f9aefa3
martin <martin@debian>
2017-06-01 02:48:48 -0600

    Se agrega interfaces

---
+++ losMejoresPrecios/src/CLASES/MAIN.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CLASES;
+
+import INTERFACES.Bienvenida;
+
+/**
+ *
+ * @author DAVID
+ */
+public class MAIN {
+    public static void main(String[] args) {
+        Bienvenida b = new Bienvenida();
+        b.setVisible(true);
+    }
+    
+}
